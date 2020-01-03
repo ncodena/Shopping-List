e7f98d207d9940e3e9360f5213ce8d6b89994fbb
Nuria <c.odena.nuria@gmail.com>
2020-01-03 11:58:53 +0100

    routes

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,10 +14,6 @@ router.get('/', (req, res) => {
         .sort({date: -1})
         .then(items => res.json(items))
 });
-router.get('/', (req, res) => {
-    Item.find()
-        .sort({date: -1})
-        .then(items => res.json(items))
-});
+
 
 module.exports = router;
