4b36396b2b5cc408c7647ebd37771c504939171f
Nuria <c.odena.nuria@gmail.com>
2020-01-05 17:10:29 +0100

    Client Setup & Navbar Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavbar from './components/appNavbar'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <AppNavbar/>
     </div>
   );
 }
