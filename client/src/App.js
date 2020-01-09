117c923ee7d0be8600bbc0a676f2fcfe7dfcebfc
Nuria <c.odena.nuria@gmail.com>
2020-01-09 11:55:59 +0100

    React & Redux Auth State

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from 'reactstrap';
 import AppNavbar from './components/appNavbar';
@@ -6,11 +6,19 @@ import ShoppingList from './components/ShoppingList';
 import ItemModal from './components/itemModal';
 import {Provider} from 'react-redux';
 import store from './store';
+import {loadUser} from './actions/authActions';
 
 
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  componentDidMount() {
+    store.dispatch(loadUser());
+  };
+
+  render(){
+
   return (
     <Provider store={store}>
       <div className="App">
@@ -23,6 +31,7 @@ function App() {
       </div>
     </Provider>
   );
+  }
 }
 
 export default App;
--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,8 @@ const jwt = require('jsonwebtoken');
 function auth (req, res, next) {
     const token = req.header('x-auth-token');
     // Check for token
-    if (!token) res.status(401).json({msg: 'No token, authorization denied'});
+    if (!token) 
+        return res.status(401).json({msg: 'No token, authorization denied'});
 
     try {
         //Verify token
@@ -13,6 +14,7 @@ function auth (req, res, next) {
 
         //Add user from payload
         req.user = decoded;
+        
         next();
 
     } catch(e){
